439960dbb93c7864d54f1471c4f92a9383772c1a
محمدولی آردین <ardin@kahkeshan.net>
2018-03-06 16:33:17 +0330

    Add project files.

---
+++ Ardin.in.Tools/PersianCalendar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ardin.@in.Tools
+{
+    public static class PersianCalendar
+    {
+        public static DateTime ToPersianCalendar(this DateTime dt)
+        {
+            return dt;
+        }
+    }
+}
